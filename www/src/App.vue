30ffcc6c81de418809eba4159ecbe1cf066a86cf
jf <jfempire@yahoo.co.uk>
2021-12-05 21:16:34 +0530

    getting started on www

---
+++ www/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import RESTVue from './components/REST.vue'
+import WebSocketVue from './components/WebSocket.vue';
+</script>
+
+<template>
+<h1>Sample Hobby App</h1>
+  <RESTVue resturl="RESTURL" />
+  <WebSocketVue websocketurl="WEBSOCKETURL" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ www/src/components/REST.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ resturl: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <h2>resturl:{{ resturl }}</h2>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
