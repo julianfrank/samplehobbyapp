b1f85e8ec41b94a980584065bb1a35ce6bb8fce9
jf <jfempire@yahoo.co.uk>
2021-12-12 23:41:51 +0530

    add server working

--- addserver/server.js
+++ addserver/server.js
@@ -7,4 +7,11 @@ app.get("/", function (req, res) {
   res.json({ add: service_discovery });
 });
 
+app.post("/add", function (req, res) {
+  console.log(req.query);
+  const { x, y } = req.query;
+  const z = Number(x) + Number(y);
+  res.json({ x, y, z });
+});
+
 app.listen(PORT, () => console.log(`ADD Server running on ${PORT}`));
--- apiserver/server.js
+++ apiserver/server.js
@@ -1,10 +1,35 @@
 const express = require("express"),
+  axios = require("axios"),
   service_discovery = require("../service_discovery.json");
 const app = express(),
   PORT = 8443 || process.env.PORT;
 
-app.get("/", function (req, res) {
+app.all("/", function (req, res) {
   res.json(service_discovery);
+});
+app.all("/add", function (req, res) {
+  const { port, hostname, pathname, protocol } = new URL(
+    service_discovery.dev.add
+  );
+  //console.log(req.query);
+  const { x, y } = req.query;
+  console.log(
+    `Trying to ADD x=${x} + y=${y} using ${hostname}:${port}${pathname}`
+  );
+
+  axios
+    .post(`${service_discovery.dev.add}/add?x=${x}&y=${y}`)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error({ error: error.toJSON() });
+      res.json({ error: error.toJSON() });
+    });
+});
+app.all("/sub", function (req, res) {
+  res.json("sub");
 });
 
 app.listen(PORT, () => console.log(`API Server running on ${PORT}`));
